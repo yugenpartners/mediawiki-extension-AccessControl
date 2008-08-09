c5008a0c0dd837133abda7578fef9ddfcae58dba
Aleš Kapica <ales.kapica@ovajih.cz>
2008-08-09 22:19:05 +0200

    Bylo umožněno použití více tagů accesscontrol na jedné stránce a doplněno systémové upozornění na to že stránka je chráněna.

--- AccessControl.php
+++ AccessControl.php
@@ -1,7 +1,6 @@
 <?php
 
-/**
- * MediaWiki extension that enables group access restriction on a page-by-page
+/* MediaWiki extension that enables group access restriction on a page-by-page
  * basis contributed by Martin Mueller (http://blog.pagansoft.de) based on
  * accesscontrol.php by Josh Greenberg
  * @package MediaWiki
@@ -18,14 +17,14 @@ if( !defined( 'MEDIAWIKI' ) ) {
 }
 
 
-// $wgAccessControlMessages = false;			// if set to true, show a line on of each secured page, which says, which groups are allowed to see this page.
+$wgAccessControlMessages = true;			// if set to true, show a line on of each secured page, which says, which groups are allowed to see this page.
 $wgUseMediaWikiGroups = true;				// use the groups from MediaWiki with Usergroup pages
 $wgAccessControlDebug = true;				// Debug log on if set to true
 $wgAccessControlDebugFile = "/var/www/debug.txt";	// Path to the debug log
 
 $wgAdminCanReadAll = true;				// sysop users can read all restricted pages
 $wgAllowUserList = true;				// Users can managed access list from userspace
-
+$wgAllowInfo = '';
 
 $wgExtensionFunctions[] = 'wfAccessControlExtension';
 $wgExtensionCredits['specialpage']['AccessControl'] = array(
@@ -54,17 +53,6 @@ $wgHooks['userCan'][] = 'hookUserCan';
 // Hook the controlUserGroupPageAccess in the Article to allow access to the "Usergroup:..." pages only to the sysop
 $wgHooks['ArticleAfterFetchContent'][] = 'controlUserGroupPageAccess';
 
-function debugme($input) {
-	/* handles the debug output to a debug file */
-	global $wgAccessControlDebug, $wgAccessControlDebugFile;
-	if ($wgAccessControlDebug) {
-		$f = fopen($wgAccessControlDebugFile, "a+");
-		fputs($f, $input."\n");
-		fclose($f);
-	}
-}
-
-
 function wfAccessControlExtension() {
 	/* This is the hook function. It adds the tag to the wiki parser
 	   and tells it what callback function to use. */
@@ -80,7 +68,8 @@ function wfAccessControlExtension() {
 
 function doControlUserAccess( $input, $argv, &$parser ) {
 	/* Funcion called by wfAccessControlExtension */
-	debugme("doControlUserAccess : start accesscontrol with input: ".$input);
+	global $wgAccessControlMessages;
+	if($wgAccessControlMessages) return displayGroups();
 }
 
 
@@ -117,10 +106,8 @@ function accessControl($groups){
 			if ($access !='edit') {
 				if (in_array($skupina, array_values($wgUser->getGroups())) === true) {
 					if ($groups[$skupina] == 'edit') {
-						debugme("accessControl 6: User ".$wgUser->getName()." is member systemgroup  '".$skupina."'");
 						$access = 'edit';
 					} elseif ($groups[$skupina] == 'read') {
-						debugme("accessControl 7: User ".$wgUser->getName()." is member systemgroup  '".$skupina."' which only read page");
 				 		$access = 'read';
 					} else {
 						// Not member in any group
@@ -157,26 +144,23 @@ function makeGroupArray($input) {
 	foreach ($groupEntry as $userGroup) {
 		if (strpos($userGroup,"(ro)") === false) {
 			$groups[$userGroup] = "edit";
-			debugme("makeGroupArray 1: set edit access for group - ".$userGroup);
 		} else {
 			$userGroup = trim(str_replace("(ro)","",$userGroup));
 			$groups[$userGroup] = "read";
-			debugme("makeGroupArray 2: set readonly access for group - ".$userGroup);
 		}
 	}
-	debugme("makeGroupArray_3: return array \$groups back into function getContentTag");
 	return $groups;
 }
 
 
-function displayGroups($groups,$from) {
+function displayGroups() {
 	/* Make the textual hyperlink group list - is not used */
-	global $wgOut;
-
-	$style = "<div id=\"accesscontrol\" style=\"color:#BA0000;font-size:8pt\">";
-	$style_end = "</div>";
-	return ( $style."kokakola".$style_end );
-	debugme("displayGroup_1: generating output from array with info about access rights groups");
+	global $wgOut, $wgAllowInfo;
+	$style = "<p id=\"accesscontrol\" style=\"text-align:center;color:#BA0000;font-size:8pt\">";
+	$text = wfMsg('accesscontrol-info');
+	$style_end = "</p>";
+	$wgAllowInfo = $style.$text.$style_end;
+	return $wgAllowInfo;
 }
 
 
@@ -200,7 +184,6 @@ function getUsersFromPages($skupina) {
 				$user = trim(str_replace("*","",$userItem));
 				$allow[$user] = 'edit';
 			} else {
-				// user může pouze číst - oříznu a vložím do pole
 				$user = trim(str_replace("*","",$userItem));
 				$user = trim(str_replace("(ro)","",$user));
 				$allow[$user] = 'read';
@@ -218,12 +201,11 @@ function getUsersFromPages($skupina) {
 function doRedirect($info) {
 	/* make redirection for non authorized users */
 	global $wgScript, $wgSitename, $wgOut;
-//	echo "<hr>doRedirect start <br>";
-//	print_r($info);
-//	echo "<br>doRedirect end<hr>";
-	// setting default No_access page
-//	echo $info;
-//	exit;
+
+	//echo "<hr>doRedirect start <br>";
+	//print_r($info);
+	//echo "<br>doRedirect end<hr>";
+
 	if (!$info) $info="No_access";
 	if ($info == "Only_sysop") {
 		$target = wfMsg('accesscontrol-info-user');
@@ -243,16 +225,13 @@ function hookUserCan( &$title, &$wgUser, $action, &$result ) {
 	/* Control read access */
 	global $wgOut;
 
-	debugme("hookUserCan_1: Go on ".$title);
 	$article = new Article( $title, 0 );
 	$content = $article->getContent();
 	$allowedGroups = getContentTag($content);
 
 	if ($action=='read') {
 		if (is_array($allowedGroups)) {
-
 			if ($allowedGroups['access'] != '') {
-				debugme("hookUserCan - read 1 : Access is set add info");
 				if($wgUser->mId == 0) {
 					doRedirect('No_anonymous 1');
 					return false;
@@ -260,20 +239,16 @@ function hookUserCan( &$title, &$wgUser, $action, &$result ) {
 				return true;
 				}
 			} elseif (count($allowedGroups['info']) < 1 ) {
-				debugme("hookUserCan - read 2 : Is not any groups, it is page for anonymous");
 				return true;
 			} else {
-				debugme("hookUserCan - read 3 : Access denied, do redirect into No_access page");
 				if ($wgUser->mId == 0) {
 					doRedirect('No_anonymous');
-					} else {
+				} else {
 					doRedirect('No_access');
 				}
 				return false;
 			}
-
 		} else {
-			debugme("hookUserCan - read 5: Any accesscontrol tag on page ".$title);
 			if($wgUser->mId == 0) {
 				return false;
 			} else {
@@ -282,17 +257,14 @@ function hookUserCan( &$title, &$wgUser, $action, &$result ) {
 		}
 	} elseif ($action=='edit') {
 		if ($allowedGroups['access'] == 'edit') {
-				debugme("hookUserCan - edit 1 : User has access right for editation");
 				return true;
 			} elseif ( count($allowedGroups['info']) < 1 ) {
-				debugme("hookUserCan - edit 2 : Is not set any groups, it is page for anonymous access");
 				if($wgUser->mId == 0) {
 					return false;
 				} else {
 					return true;
 				}
 			} else {
-				debugme("hookUserCan - edit 3 : Editation for this user is denied");
 				return false;
 			}
 	} elseif ($action=='move') {
@@ -306,33 +278,52 @@ function hookUserCan( &$title, &$wgUser, $action, &$result ) {
 	}
 }
 
-
-function getContentTag($content) {
-	/* Function for get content from accesscontrol element. */
+function allRightTags($string) {
+	/* Function for extraction content tag accesscontrol */
+	$contenttag=Array();
 	$starttag = "<accesscontrol>";
 	$endtag = "</accesscontrol>";
-	$start = strpos( $content, $starttag );
+	$start = strpos( $string, $starttag );
 	if ($start != false) {
 		$start += strlen($starttag);
-		$end = strpos( $content, $endtag );
+		$end = strpos( $string, $endtag );
 		if ($end != false) {
-			$groupsString = substr($content, $start, $end-$start );
-			debugme("getContentTag_1: substring '".$groupsString."' forward into function makeGroupArray" );
-			$groups = makeGroupArray($groupsString);
-			debugme("getContentTag_2: Vrátil se seznam skupin, je třeba ho vyhodnotit" );
-			$message= accessControl($groups);
-			if ($message) {
-				return $message;
+			$groupsString = substr($string, $start, $end-$start );
+			if (strlen($groupsString) == 0) {
+				$contenttag['end'] = strlen($starttag)+strlen($endtag); 
 			} else {
-				debugme("getContentTag_3: Stránka nemá nastavený žádný seznam skupin s právem přístupu" );
-				return true;
+				$contenttag['groups']=$groupsString;
+				$contenttag['end'] = $end+strlen($endtag);
 			}
-		} else {
-			debugme("getContentTag_4: Na stránce chybí ukončovací tag" );
-			return true;
+			return $contenttag;
 		}
 	} else {
-		debugme("getContentTag_5: Stránka nemá nijak omezená přístupová práva");
+		return false;
+	}
+}
+
+function getContentTag($content) {
+	/* Function get content from all accesscontrol elements in page. */
+	$start = 0;
+	$groupsString = '';
+	while ( strlen($content) > 0 ) {
+		$obsah = allRightTags($content);
+		if(count($obsah)>1) {
+			$start = $obsah['end'];
+			} else {
+			//print "Jsem tu<hr>";
+			$start = 31;
+			}
+		if (isset($obsah['groups']) && strlen($groupsString) > 0) $groupsString .= ',';
+		$groupsString .= $obsah['groups'];
+		$content=substr($content, $start);
+	}
+	if(strlen($groupsString) == 0) return true;
+	$groups = makeGroupArray($groupsString);
+	$message= accessControl($groups);
+	if ($message) {
+		return $message;
+	} else {
 		return true;
 	}
 }
